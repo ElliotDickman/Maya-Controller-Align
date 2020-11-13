fcc05fefd943d15c5ef9479e30e457921348edf0
ElliotDickman <56144216+ElliotDickman@users.noreply.github.com>
2020-11-13 16:03:47 -0500

    Update AlignController.py

--- AlignController.py
+++ AlignController.py
@@ -1,14 +1,24 @@
+### Select all controllers then all joints to align
+### Created by Elliot Dickman - 2020 - Maya 2018
+
 import maya.cmds as cmds
 
 # Get selection
 sel = cmds.ls( orderedSelection=True )
-targetCtrl = sel[0]
-targetJoint = sel[1]
+targetCtrl = []
+targetJoint = []
 
-cmds.parent(targetCtrl, targetJoint)
+for i in sel:
+    if (cmds.objectType(i, isType="joint")):
+        targetJoint.append(i)
+    else:
+        targetCtrl.append(i)
 
-cmds.xform(targetCtrl, translation=[0,0,0], rotation=[0,0,0])
+for j in range(len(targetJoint)):
+    cmds.parent(targetCtrl[j], targetJoint[j])
 
-cmds.parent(targetCtrl, world=True)
+    cmds.xform(targetCtrl[j], translation=[0,0,0], rotation=[0,0,0])
+
+    cmds.parent(targetCtrl[j], world=True)
 
 print "Aligned %s to %s\n" % (targetCtrl, targetJoint)
